c1c84036c839e18eeb7a39385bdd4be466340251
Ege Uz <enkomaun@gmail.com>
2019-12-11 12:03:45 -0500

    production build

---
+++ build/precache-manifest.f78c8708bd9fc6eab1e43b54765291b6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c48996cd8715d4856fa8151eda353ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05daee84af7247890ec3",
+    "url": "/static/css/main.4a3d0663.chunk.css"
+  },
+  {
+    "revision": "a7baec6255f5f36db273",
+    "url": "/static/js/2.0698ffbf.chunk.js"
+  },
+  {
+    "revision": "05daee84af7247890ec3",
+    "url": "/static/js/main.41f0b95f.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "a511797ae027190e3ca23129583bade1",
+    "url": "/static/media/bed.a511797a.png"
+  },
+  {
+    "revision": "349d876834edff80cf51c33c17bb5390",
+    "url": "/static/media/chair.349d8768.png"
+  },
+  {
+    "revision": "b692e9fe0f6e6c3ca40031496efc9f61",
+    "url": "/static/media/clock.b692e9fe.png"
+  },
+  {
+    "revision": "ced5912abd9ad8e52b5053ddf62e90aa",
+    "url": "/static/media/couch.ced5912a.png"
+  },
+  {
+    "revision": "1a80620af0cbc65253c2b8556e48b9f3",
+    "url": "/static/media/doll.1a80620a.png"
+  },
+  {
+    "revision": "d324ab9c9dba97f12e396ee7ac434e05",
+    "url": "/static/media/flowers.d324ab9c.png"
+  },
+  {
+    "revision": "686a277bf6309ce42f4efad04b137b98",
+    "url": "/static/media/piano.686a277b.png"
+  },
+  {
+    "revision": "be15d9de2288c9c8a50b554a0c2e2877",
+    "url": "/static/media/purse.be15d9de.png"
+  },
+  {
+    "revision": "8dc5b7be92c98b5eec39c08784edbd8e",
+    "url": "/static/media/room.8dc5b7be.png"
+  },
+  {
+    "revision": "eda3f59a4aa0eee568cbf83f2fb46fcd",
+    "url": "/static/media/table.eda3f59a.png"
+  },
+  {
+    "revision": "401bc5db5b437b3abb631dfdb4d75ff9",
+    "url": "/static/media/television.401bc5db.png"
+  },
+  {
+    "revision": "180107bf62599ffda098dc55845cfc6f",
+    "url": "/static/media/terminal.180107bf.png"
+  },
+  {
+    "revision": "9dc3eac8918337168ed53d51eb0adbc8",
+    "url": "/static/media/terminal.9dc3eac8.png"
+  },
+  {
+    "revision": "0452652fd9888bd789e6114203ec6da2",
+    "url": "/static/media/toilet.0452652f.png"
+  },
+  {
+    "revision": "aa4603e24ea361a30f074f813b3614c6",
+    "url": "/static/media/vase.aa4603e2.png"
+  }
+]);
